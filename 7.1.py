4b490bda5fb1c4ab7b78faa3ccf68b49a21bf9f9
Anton1Ilyin <114080742+Anton1Ilyin@users.noreply.github.com>
2022-10-26 11:51:30 +0300

    Complete 7.1

--- 7.1.py
+++ 7.1.py
@@ -4,58 +4,72 @@ import matplotlib.pyplot as plt
 GPIO.setmode(GPIO.BCM)
 dac=[26, 19,13,6,5,11,9,10]
 leds=[21,20,16,12,7,8,25,24]
-GPIO.setup(dac ,GPIO.OUT)
-[GPIO.setup(x,GPIO.OUT) for x in leds]
+GPIO.setup(dac ,GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(leds,GPIO.OUT)
 comp=4
 troyka=17
-GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(troyka, GPIO.OUT)
 GPIO.setup(comp, GPIO.IN)
+
 #перевод из десятичной в двоичную
 def d2b(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
+
 #измерение напряжения на выходе тройка-модуля
 def adc():
     value=0
-    v=0
-    for i in range(8):
-        v=value+2**(7-i)
-        if v<256:
-            a=d2b(v)
-        GPIO.output(dac,v)
+    for i in range(7, -1, -1):
+        value+2**(7-i)
+        a=d2b(value)
+        GPIO.output(dac,a)
         time.sleep(0.0007)
-        if GPIO.input(comp)==1:
-            value=v
+        if GPIO.input(comp)==0:
+            value-=2**i
     return value
+
 #вывод на значения напряжения на leds
 def led(v0):
-    v=d2b(v0)
-    [GPIO.output(leds[x],v[x]) for x in range(len(leds))]
+    vi=d2b(v0)
+    GPIO.output(leds,vi)
+    
 try:
     measured_data=[]
     start_time=time.time()
     GPIO.output(troyka, 1)
-    vi=adc()
-    while (vi<0.97*3.3):
-        measured_data.append(vi)
-        vi=adc()
-        led(vi)
+    v=0
+    
+    #конденсатор заряжается
+    while (v<0.97*256):
+        v=adc()
+        measured_data.append(v)
+        led(v)
+        
     GPIO.output(troyka,0)
-    vi=adc()
-    while (vi>0.02*3.3):
-        measured_data.append(vi)
-        vi=adc()
-        led(vi)
+    
+    #конденсатор разряжается
+    while (v>0.02*256):
+        v=adc()
+        measured_data.append(v)
+        led(v)
+        
     end_time=time.time()
     experiment_time=end_time-start_time
-    plt.scatter(range(len(measured_data)), measured_data)
+    
+    #Построение графиков
+    plt.plot(range(len(measured_data)), [x*3.3/256 for i in measured_data])
     plt.show()
+    
     measured_data_str=[str(item) for item in measured_data]
+    
+    #создание файлов с данными эксперимента
     with open("data.txt", "w") as file:
         file.write("\n".join(measured_data_str))
     frequency=len(measured_data)/experiment_time
     stroka="Шаг квантования АЦП "+str(3.3/256)+"\n"+"Средняя частота дискретизации"+str(frequency)
     with open("settings.txt", "w") as f:
-        f.write(stroka)
+        f.write(str(3.3/256)+'\n')
+        f.write(str(frequency))
+        
     print("Время эксперимента", experiment_time)
     print("Период одного измерния",1/frequency)
     print("Средняя частота дискретизации", frequency)
